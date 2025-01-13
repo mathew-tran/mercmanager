0e964da278fd4ce1926ece2270de27f288304318
Mathew Tran <mathew.tran.dev@gmail.com>
2025-01-12 17:10:51 -0800

    worked a bit on merc manager

---
+++ Scripts/Data/DataType/Stats.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name Stats
+
+@export var HP = 1
+@export var Damage = 1
+@export var Defense = 1
+@export var Speed = 1
+@export var Crit = 0
---
+++ Scripts/Data/DataType/Unit.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name Unit
+
+@export var Name = "test"
+@export var Face : Texture2D
+@export var StatValues : Stats
