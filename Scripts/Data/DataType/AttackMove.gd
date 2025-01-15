5e147d997b135ca0d9210a4c81acbd9af9df53dc
Mathew Tran <mathew.tran.dev@gmail.com>
2025-01-14 19:29:35 -0800

    make telegraphing a bit better

--- Scripts/Data/DataType/AttackMove.gd
+++ Scripts/Data/DataType/AttackMove.gd
@@ -6,18 +6,22 @@ class_name AttackMove
 @export var HitAmount = 1
 
 func Execute(owner : Character, targets : Array[Character]):
-	for x in range(0, HitAmount):
-		for target in targets:
-			if HasMoveSucceeded():
-				await owner.get_tree().create_timer(.1).timeout
-				await owner.Speak("")
-				if is_instance_valid(Effect):
-					await Effect.PlayEffect(owner, target)
-				target.GetHealthComponent().TakeDamage(CalculateDamage(owner))
-				await target.get_tree().create_timer(.2).timeout
-			else:
-				await target.get_tree().create_timer(.2).timeout
-				
+	if HasMoveSucceeded():
+		for x in range(0, HitAmount):
+			for target in targets:
+				if HasMoveSucceeded() or x == 0:
+					await owner.get_tree().create_timer(.1).timeout
+					await owner.Speak("")
+					if is_instance_valid(Effect):
+						await Effect.PlayEffect(owner, target)
+					target.GetHealthComponent().TakeDamage(CalculateDamage(owner))
+					await target.get_tree().create_timer(.2).timeout
+				else:
+					await target.get_tree().create_timer(.2).timeout
+	else:
+		await owner.Speak("MISSED")
+		await owner.get_tree().create_timer(.4).timeout
+		await owner.Speak("")		
 
 func CalculateDamage(owner: Character):
 	var damage = BaseDamage	
